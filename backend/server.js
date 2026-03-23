356f4bb3576621fc3ae939745fd443ac21bf2da5
Sixtus-Dev7 <cjsixt@gmail.com>
2026-03-23 22:01:19 +0100

    fix: add start script

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json);
 
-app.use("/api/v1/reviews", reviewss);
+app.use("/api/v1/reviews", reviews);
 
 app.use((req, res) => {
   res.status(404).json({ error: "not found" });
